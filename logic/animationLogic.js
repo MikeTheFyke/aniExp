e6842a3659568f1d46c36aebc758892a055832be
MikeTheFyke <mikefyke@hotmail.com>
2020-10-06 14:59:59 -0400

    p tag is animating y and opacity

--- logic/animationLogic.js
+++ logic/animationLogic.js
@@ -1,7 +1,7 @@
 document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.panel', 1.5, { scaleY: 1, height: '100vh', ease: Circ.easeOut } );
     TweenMax.to('#bullseye', 1, { opacity: 1, y: 0, delay: 1, ease: Bounce.easeOut } );
-
+    TweenMax.to('p', 1, { opacity: 1, y: 20, delay: 1.4 } );
 })
 
 
