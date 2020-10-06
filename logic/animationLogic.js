34743a4f7545ce6bf4247ad9bfad68392275ead4
MikeTheFyke <mikefyke@hotmail.com>
2020-10-06 14:57:17 -0400

    gsap panel and img are easing

--- logic/animationLogic.js
+++ logic/animationLogic.js
@@ -1,3 +1,9 @@
 document.getElementById('cta').addEventListener('click', function () {
-    TweenMae.to()
+    TweenMax.to('.panel', 1.5, { scaleY: 1, height: '100vh', ease: Circ.easeOut } );
+    TweenMax.to('#bullseye', 1, { opacity: 1, y: 0, delay: 1, ease: Bounce.easeOut } );
+
 })
+
+
+// TweenMax.to('SELECTOR', DURATION, { CSS TAG ELEMENTS TO ANIMATE } )
+// https://greensock.com/docs/v3/Eases
