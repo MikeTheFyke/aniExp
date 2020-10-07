98b05cf28b0f90aea2ff42d6ca31223dc156351a
MikeTheFyke <mikefyke@hotmail.com>
2020-10-06 21:53:28 -0400

    Added comments to gsap function

--- logic/animationLogic.js
+++ logic/animationLogic.js
@@ -2,6 +2,8 @@ TweenMax.to('h1', 1, { opacity: 1, scale: 1 } );
 TweenMax.to('#cta', 1, { opacity: 1, scale: 1 } );
 TweenMax.to('#cta2', 1, { opacity: 1, scale: 1 } );
 
+// Button 1 animations
+
 document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.panel', 1.5, { scaleY: 1, height: '100vh', ease: Circ.easeOut } );
     TweenMax.to('#bullseye', 1, { opacity: 1, y: 0, delay: 1, ease: Bounce.easeOut } );
@@ -16,6 +18,8 @@ document.getElementById('cta').addEventListener('mouseover', function(){
 document.getElementById('cta').addEventListener('mouseout', function(){
     TweenMax.to('#cta', 1.5, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
 });
+
+// Button 2 animations
 
 document.getElementById('cta2').addEventListener('mouseover', function(){
     TweenMax.to('#cta2', 1.5, { scale: 2, ease: Elastic.easeOut.config(1, 0.3) } );
