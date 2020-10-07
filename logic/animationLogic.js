a3215f38a53a671478f5317305e41ed7b93beb54
MikeTheFyke <mikefyke@hotmail.com>
2020-10-06 22:00:55 -0400

    Made p element move diagonally

--- logic/animationLogic.js
+++ logic/animationLogic.js
@@ -7,7 +7,7 @@ TweenMax.to('#cta2', 1, { opacity: 1, scale: 1 } );
 document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.panel', 1.5, { scaleY: 1, height: '100vh', ease: Circ.easeOut } );
     TweenMax.to('#bullseye', 1, { opacity: 1, y: 0, delay: 1, ease: Bounce.easeOut } );
-    TweenMax.to('p', 1, { opacity: 1, y: 20, delay: 1.4 } );
+    TweenMax.to('p', 1, { opacity: 1, y: 20, x:50, delay: 1.4 } );
     TweenMax.to('.box', 3, { opacity: 1, scale: 1, ease: Elastic.easeOut.config(1, 0.3) });
 })
 
@@ -31,10 +31,14 @@ document.getElementById('cta2').addEventListener('mouseout', function(){
 
 document.getElementById('cta2').addEventListener('click', function () {
     TweenMax.to('.box', 3, { opacity: 0, scale: 0, ease: Elastic.easeOut.config(1, 0.3) });
-    TweenMax.to('p', 1, { opacity: 0, y: -20 } );
+    TweenMax.to('p', 1, { opacity: 0, y: -20, x:-50 } );
     TweenMax.to('#bullseye', 1, { opacity: 0, y: -200, ease: Circ.easeOut } );
     TweenMax.to('.panel', 1.5, { scaleY: 0, delay: 0.75, ease: Circ.easeOut } );
 })
+
+///
+
+
 
 // TweenMax.to('SELECTOR', DURATION, { CSS TAG ELEMENTS TO ANIMATE } )
 // https://greensock.com/docs/v3/Eases
