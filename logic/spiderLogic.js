fd44f86701974ceeb10adc26fe8e79bee070dec4
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 12:14:14 -0400

    Updated spider-container postion and size

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -1,5 +1,5 @@
-TweenMax.to('.main-container', 1, { opacity:1} );
-TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
-TweenMax.to('.main-container', 1, { background: 'crimson', delay: 1 } );
-
-
+document.getElementById('cta').addEventListener('click', function () {
+    TweenMax.to('.main-container', 1, { opacity:1} );
+    TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
+    TweenMax.to('.main-container', 1, { background: 'crimson', delay: 1 } );    
+})
