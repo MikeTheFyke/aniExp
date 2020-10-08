80eb70e2b0d59441c42fe88fa5c5e54c83a527f5
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 12:21:06 -0400

    gsap animating cta spider button

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -1,3 +1,5 @@
+TweenMax.to('#cta', 1, { opacity:1, y:0, delay: 1.4} );
+
 document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.main-container', 1, { opacity:1} );
     TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
