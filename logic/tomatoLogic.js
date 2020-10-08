4a1291964d81cd3e8a5d5d7ed9ce2afc38f6b0b4
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 11:27:17 -0400

    Created spiderstyles

--- logic/tomatoLogic.js
+++ logic/tomatoLogic.js
@@ -7,9 +7,9 @@ TweenMax.to('.bottom-panel', 1, { scaleY:1, height: '75vh', ease: Circ.easeOut }
 // Button 1 animations
 
 document.getElementById('cta').addEventListener('click', function () {
-    TweenMax.to('#tomato1', 1, { opacity: 1, y:0, delay: 1, ease: Elastic.easeOut.config(1, 0.3) } );
-    TweenMax.to('#tomato2', 1, { opacity: 1, y:0, delay: 1.25, ease: Elastic.easeOut.config(1, 0.3) } );
-    TweenMax.to('#tomato3', 1, { opacity: 1, y:0, delay: 1.50, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato1', 1, { opacity: 1, y:0, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato2', 1, { opacity: 1, y:0, delay: .25, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato3', 1, { opacity: 1, y:0, delay: .50, ease: Elastic.easeOut.config(1, 0.3) } );
 })
 
 document.getElementById('cta').addEventListener('mouseover', function(){
