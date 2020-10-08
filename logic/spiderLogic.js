6a96baa0014e8eed96c5a246117b806b199a8888
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 13:01:36 -0400

    Adjusted timing for cta animations

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -8,9 +8,9 @@ TweenMax.to('#cta', 1, { scaleX: 0, delay: 4} );
 TweenMax.to('#cta', 1, { scaleX: 1, delay: 5} );
 
 document.getElementById('cta').addEventListener('click', function () {
-    TweenMax.to('h1', 0.25, { opacity:0, scaleY:0, scaleX:0 } );
-    TweenMax.to('.main-container', 1, { opacity:1} );
-    TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
-    TweenMax.to('.main-container', 1, { background: 'crimson', delay: 1 } );    
-    TweenMax.to('#cta', 0.25, { opacity:0, y:'-50vh', delay: 1} );
+    TweenMax.to('h1', 0.5, { opacity:0, scaleY:0, scaleX:0 } );
+    TweenMax.to('.main-container', 0, { opacity:1} );
+    TweenMax.to('.panel', 0.25, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
+    TweenMax.to('.main-container', 0.25, { background: 'crimson', delay: .25 } );    
+    TweenMax.to('#cta', 0.25, { opacity:0, y:'-50vh', delay: .5} );
 })
