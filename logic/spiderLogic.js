38cd56cf86a62847f1378b5cc2f31f9bd197a760
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 14:21:47 -0400

    2nd stripe gsap complete and starting 3 amd 4

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -13,5 +13,6 @@ document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.panel', 0.25, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
     TweenMax.to('.main-container', 0.25, { background: 'crimson', delay: .25 } );    
     TweenMax.to('#cta', 0.25, { opacity:0, y:'-50vh', delay: .5} );
-    TweenMax.to('.stripe1', 0.5, { opacity:1, x:-100, delay: .5 } )
+    TweenMax.to('#stripe1', 3, { opacity:1, x:-200, delay: 2 } );
+    TweenMax.to('#stripe2', 3, { opacity:1, x:-400, delay: 2 } );
 })
