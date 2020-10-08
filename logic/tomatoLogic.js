c8651051790b0db48f205da5dfe4f5719b7b0f15
MikeTheFyke <mikefyke@hotmail.com>
2020-10-07 22:03:00 -0400

    tomatoes are set up and knocking down

--- logic/tomatoLogic.js
+++ logic/tomatoLogic.js
@@ -4,12 +4,12 @@ TweenMax.to('#cta', 1, { opacity: 1, x:0, delay: 1 } );
 TweenMax.to('#cta2', 1, { opacity: 1, x: 0, delay: 1 } );
 
 TweenMax.to('.bottom-panel', 1, { scaleY:1, height: '75vh', ease: Circ.easeOut } );
-TweenMax.to('#tomato1', 1, { opacity: 1, y:0, delay: 1, ease: Elastic.easeOut.config(1, 0.3) } );
-TweenMax.to('#tomato2', 1, { opacity: 1, y:0, delay: 1.25, ease: Elastic.easeOut.config(1, 0.3) } );
-TweenMax.to('#tomato3', 1, { opacity: 1, y:0, delay: 1.50, ease: Elastic.easeOut.config(1, 0.3) } );
 // Button 1 animations
 
 document.getElementById('cta').addEventListener('click', function () {
+    TweenMax.to('#tomato1', 1, { opacity: 1, y:0, delay: 1, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato2', 1, { opacity: 1, y:0, delay: 1.25, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato3', 1, { opacity: 1, y:0, delay: 1.50, ease: Elastic.easeOut.config(1, 0.3) } );
 })
 
 document.getElementById('cta').addEventListener('mouseover', function(){
@@ -30,6 +30,9 @@ document.getElementById('cta').addEventListener('mouseout', function(){
 
 // Button 2 animations
 document.getElementById('cta2').addEventListener('click', function () {
+    TweenMax.to('#tomato1', 1, { y:+200, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato2', 1, { y:+200, delay: .25, ease: Elastic.easeOut.config(1, 0.3) } );
+    TweenMax.to('#tomato3', 1, { y:+200, delay: .50, ease: Elastic.easeOut.config(1, 0.3) } );
 })
 
 document.getElementById('cta2').addEventListener('mouseover', function(){
