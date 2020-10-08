cef4cc52921dcf3ec27b0149c4087689d2525347
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 12:53:14 -0400

    adjusted h1 gsap speed

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -1,5 +1,5 @@
 TweenMax.to('#cta', 1, { opacity:1, y:0, delay: 1.4} );
-TweenMax.to('h1', 1, { opacity:1, y:0, delay: 1.4} );
+TweenMax.to('h1', 1, { opacity:1, scaleY:1, scaleX:1, delay: 2} );
 
 TweenMax.to('#cta', 1, { scaleX: 0, delay: 2} );
 TweenMax.to('#cta', 1, { scaleX: 1, delay: 3} );
@@ -8,8 +8,9 @@ TweenMax.to('#cta', 1, { scaleX: 0, delay: 4} );
 TweenMax.to('#cta', 1, { scaleX: 1, delay: 5} );
 
 document.getElementById('cta').addEventListener('click', function () {
+    TweenMax.to('h1', 0.25, { opacity:0, scaleY:0, scaleX:0 } );
     TweenMax.to('.main-container', 1, { opacity:1} );
     TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
     TweenMax.to('.main-container', 1, { background: 'crimson', delay: 1 } );    
-    TweenMax.to('#cta', 1, { opacity:0, y:'-50vh', delay: 1} );
+    TweenMax.to('#cta', 0.25, { opacity:0, y:'-50vh', delay: 1} );
 })
