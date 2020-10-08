5bddec3a398319026643e63f93b88a0df7b0a69f
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 14:29:26 -0400

    6 left stripes are gsap animationg

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -13,6 +13,10 @@ document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.panel', 0.25, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
     TweenMax.to('.main-container', 0.25, { background: 'crimson', delay: .25 } );    
     TweenMax.to('#cta', 0.25, { opacity:0, y:'-50vh', delay: .5} );
-    TweenMax.to('#stripe1', 3, { opacity:1, x:-200, delay: 2 } );
-    TweenMax.to('#stripe2', 3, { opacity:1, x:-400, delay: 2 } );
+    TweenMax.to('#stripe1', 1, { opacity:1, x:'-10vw', delay: 2 } );
+    TweenMax.to('#stripe2', 2, { opacity:1, x:'-20vw', delay: 2.25 } );
+    TweenMax.to('#stripe3', 2, { opacity:1, x:'-28vw', delay: 2.50 } );
+    TweenMax.to('#stripe4', 3, { opacity:1, x:'-35vw', delay: 2.75 } );
+    TweenMax.to('#stripe5', 3, { opacity:1, x:'-41vw', delay: 3 } );
+    TweenMax.to('#stripe6', 3, { opacity:1, x:'-46vw', delay: 3.25 } );
 })
