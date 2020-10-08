87285cba26f4cf589c5cb3c5d9d4922f72ddfba4
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 11:53:50 -0400

    Created panel and body gsap animation

---
+++ logic/spiderLogic.js
@@ -0,0 +1,5 @@
+TweenMax.to('.main-container', 1, { opacity:1} );
+TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
+TweenMax.to('.main-container', 1, { background: 'crimson', delay: 1 } );
+
+
