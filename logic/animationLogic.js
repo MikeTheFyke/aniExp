c52933604490b30cac18f98e19721d1a7deb58b8
MikeTheFyke <mikefyke@hotmail.com>
2020-10-07 21:27:22 -0400

    Top panel is sliding down and buttons are expanding

--- logic/animationLogic.js
+++ logic/animationLogic.js
@@ -1,7 +1,6 @@
 TweenMax.to('h1', 1, { opacity: 1, scale: 1 } );
 TweenMax.to('#cta', 1, { opacity: 1, scale: 1 } );
 TweenMax.to('#cta2', 1, { opacity: 1, scale: 1 } );
-TweenMax.to('.footer', 1, { opacity: 1, scale: 1 } );
 
 // Button 1 animations
 
@@ -36,83 +35,6 @@ document.getElementById('cta2').addEventListener('click', function () {
     TweenMax.to('#bullseye', 1, { opacity: 0, y: -200, ease: Circ.easeOut } );
     TweenMax.to('.panel', 1.5, { scaleY: 0, delay: 0.75, ease: Circ.easeOut } );
 })
-
-/// Link1 button animations
-document.getElementById('tomato-link').addEventListener('click', function () {
-    this.style.color = "white";
-    this.style.backgroundColor = "crimson";
-});
-
-document.getElementById('tomato-link').addEventListener('mouseover', function(){
-    TweenMax.to('#tomato-link', 1.5, { scale: 1.5, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "red";
-    this.style.color = "crimson";
-    this.style.backgroundColor = "white";
-});
-
-document.getElementById('tomato-link').addEventListener('mouseout', function(){
-    TweenMax.to('#tomato-link', 1.5, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "black";
-    this.style.color = "black";
-    this.style.backgroundColor = "transparent";
-});
-/// Link2 button animations
-document.getElementById('can-link').addEventListener('click', function () {
-    this.style.color = "white";
-    this.style.backgroundColor = "limegreen";
-});
-
-document.getElementById('can-link').addEventListener('mouseover', function(){
-    TweenMax.to('#can-link', 1.5, { scale: 1.5, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "green";
-    this.style.color = "limegreen";
-    this.style.backgroundColor = "white";
-});
-
-document.getElementById('can-link').addEventListener('mouseout', function(){
-    TweenMax.to('#can-link', 1.5, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "black";
-    this.style.color = "black";
-    this.style.backgroundColor = "transparent";
-});
-/// Link3 button animations
-document.getElementById('bottle-link').addEventListener('click', function () {
-    this.style.color = "white";
-    this.style.backgroundColor = "lightsteelblue";
-});
-
-document.getElementById('bottle-link').addEventListener('mouseover', function(){
-    TweenMax.to('#bottle-link', 1.5, { scale: 1.5, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "blue";
-    this.style.color = "lightsteelblue";
-    this.style.backgroundColor = "white";
-});
-
-document.getElementById('bottle-link').addEventListener('mouseout', function(){
-    TweenMax.to('#bottle-link', 1.5, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "black";
-    this.style.color = "black";
-    this.style.backgroundColor = "transparent";
-});
-/// Link4 button animations
-document.getElementById('apple-link').addEventListener('click', function () {
-    this.style.color = "white";
-    this.style.backgroundColor = "goldenrod";
-});
-
-document.getElementById('apple-link').addEventListener('mouseover', function(){
-    TweenMax.to('#apple-link', 1.5, { scale: 1.5, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "orange";
-    this.style.color = "goldenrod";
-    this.style.backgroundColor = "white";
-});
-
-document.getElementById('apple-link').addEventListener('mouseout', function(){
-    TweenMax.to('#apple-link', 1.5, { scale: 1, ease: Elastic.easeOut.config(1, 0.3) } );
-    this.style.borderColor = "black";
-    this.style.color = "black";
-    this.style.backgroundColor = "transparent";
-});
 
 // TweenMax.to('SELECTOR', DURATION, { CSS TAG ELEMENTS TO ANIMATE } )
 // https://greensock.com/docs/v3/Eases
