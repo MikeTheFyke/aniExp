53885c116947c6196ed64a05f8730d0310525610
MikeTheFyke <mikefyke@hotmail.com>
2020-10-07 21:35:15 -0400

    Second button is sliding in

--- logic/tomatoLogic.js
+++ logic/tomatoLogic.js
@@ -1,4 +1,4 @@
 TweenMax.to('.top-panel', 1, { scaleY:1, height: '20vh', ease: Circ.easeIn } );
 TweenMax.to('h1', 1, { opacity: 1, scale: 1, delay: 1 } );
-TweenMax.to('#cta', 1, { x:0, delay: 1 } );
-TweenMax.to('#cta2', 1, { opacity: 1, scale: 1, delay: 1 } );
+TweenMax.to('#cta', 1, { opacity: 1, x:0, delay: 1 } );
+TweenMax.to('#cta2', 1, { opacity: 1, x: 0, delay: 1 } );
