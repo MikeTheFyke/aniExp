268f6b690a625e4f698f2405c58fce0fc1f7a6db
MikeTheFyke <mikefyke@hotmail.com>
2020-10-08 12:44:56 -0400

    h1 and spider gsap loading together

--- logic/spiderLogic.js
+++ logic/spiderLogic.js
@@ -1,4 +1,5 @@
 TweenMax.to('#cta', 1, { opacity:1, y:0, delay: 1.4} );
+TweenMax.to('h1', 1, { opacity:1, y:0, delay: 1.4} );
 
 TweenMax.to('#cta', 1, { scaleX: 0, delay: 2} );
 TweenMax.to('#cta', 1, { scaleX: 1, delay: 3} );
@@ -10,4 +11,5 @@ document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to('.main-container', 1, { opacity:1} );
     TweenMax.to('.panel', 1, { scaleY:1, scaleX:1, marginTop: 0, width: '100vw', height: '100vh', ease: Circ.easeIn } );
     TweenMax.to('.main-container', 1, { background: 'crimson', delay: 1 } );    
+    TweenMax.to('#cta', 1, { opacity:0, y:'-50vh', delay: 1} );
 })
