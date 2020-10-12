051df75c1e08469072e6b122fe03a36de749ea5f
MikeTheFyke <mikefyke@hotmail.com>
2020-10-11 21:06:22 -0400

    bulletSteak styles

--- logic/archerLogic.js
+++ logic/archerLogic.js
@@ -52,5 +52,6 @@ document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to("#blueRight3", 1, { x:'100vw', delay: 10.5 } );
     TweenMax.to("#blueRight4", 1, { x:'100vw', delay: 10.5 } );
     TweenMax.to("#mikefyke-text", 1, { x:'100vw', delay: 10.5 } );
+    
     TweenMax.to(".bulletDivs", 2, { x: '450vw', delay: 11 } );
 })
