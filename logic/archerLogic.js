0d46f7127ef3f7212e093cb4fa33714323defa38
MikeTheFyke <mikefyke@hotmail.com>
2020-10-13 11:26:05 -0400

    Clockface endpoint gsap glass div

--- logic/archerLogic.js
+++ logic/archerLogic.js
@@ -121,6 +121,6 @@ document.getElementById('cta').addEventListener('click', function () {
     TweenMax.to("#bulletStreak", 0.75, { x:'300px', scaleY: 2, delay: 26.5})
 
     TweenMax.to("#glassDiv", 1, { x:'2200px', y:'-400px', scaleX:0.5, scaleY: 0.5, delay: 26.5})
-    TweenMax.to("#clockFace", 0.5, {x:'-800px', scaleX: 0.05, scaleY: 0.05, delay: 26.5});
+    TweenMax.to("#clockFace", 0.5, {x:'-850px', scaleX: 0.05, scaleY: 0.05, delay: 26.5});
     TweenMax.to("#walter-text", 1, { y:'-825px', delay: 26.5})
 })
