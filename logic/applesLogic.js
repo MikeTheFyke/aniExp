b7678d73f8667b8e0ab899536ae9988f7cf0eb49
MikeTheFyke <mikefyke@hotmail.com>
2020-10-19 19:53:01 -0400

    Added scale X transform to all covers

--- logic/applesLogic.js
+++ logic/applesLogic.js
@@ -1,4 +1,4 @@
 TweenMax.to("#title-nemesis", 0.5, { y: '410px', delay: 0.5});
 TweenMax.to("#title-authors", 0.5, { y: '100px', delay: 1});
 
-TweenMax.to("#cta1", 0.5, { scaleX: 1, delay: 1.5});
+TweenMax.to(".cta", 0.5, { scaleX: 1, delay: 1.5});
