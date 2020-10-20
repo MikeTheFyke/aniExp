89501353c9bb3ef719dacef12273d3a4b47b143f
MikeTheFyke <mikefyke@hotmail.com>
2020-10-20 15:38:18 -0400

    Added reset to cover 1 on click title click

--- logic/applesLogic.js
+++ logic/applesLogic.js
@@ -8,7 +8,7 @@ document.getElementById('cta1').addEventListener('click', function () {
     TweenMax.to("#cta3", 1, { x: '-40vw', opacity:0})
     TweenMax.to("#cta4", 1, { x: '-60vw', opacity:0})
     TweenMax.to("#cta1", 1, { x: window.screen.width / 1.75, delay: 1.5})
-    TweenMax.to("#issue1-cover", 1, { opacity: 1, scaleX: 2, scaleY: 1.5, delay: 2.5});
+    TweenMax.to("#issue1-cover", 1, { opacity: 1, y: -100, scaleX: 2, scaleY: 1.5, delay: 2.5});
     TweenMax.to("#cta1", 1, { opacity: 0, delay: 2.5})
     TweenMax.to(".buttons-container", 1, { zIndex: 0, delay: 2.5})
 })
@@ -35,8 +35,9 @@ document.getElementById('cta4').addEventListener('click', function () {
 })
 
 document.getElementById('title-nemesis').addEventListener('click', function () {
-    TweenMax.to("#cta1", 1, { x: '0', opacity:1})
-    TweenMax.to("#cta2", 1, { x: '0',opacity:1})
-    TweenMax.to("#cta3", 1, { x: '0',opacity:1})
-    TweenMax.to("#cta4", 1, { x: '0',opacity:1})
+    TweenMax.to("#cta1", 1, { x: '0', opacity:1, zIndex: 1})
+    TweenMax.to("#cta2", 1, { x: '0',opacity:1, zIndex: 1})
+    TweenMax.to("#cta3", 1, { x: '0',opacity:1, zIndex: 1})
+    TweenMax.to("#cta4", 1, { x: '0',opacity:1, zIndex: 1})
+    TweenMax.to("#issue1-cover", 1, { opacity: 0, y: 0, scaleX: 1, scaleY: 1})
 })
