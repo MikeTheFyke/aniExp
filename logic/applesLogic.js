5b4b9fd7f04fe25677c84baf0ed592140f674cd1
MikeTheFyke <mikefyke@hotmail.com>
2020-10-19 20:21:06 -0400

    Added slide to middle to selected cta button

--- logic/applesLogic.js
+++ logic/applesLogic.js
@@ -7,24 +7,28 @@ document.getElementById('cta1').addEventListener('click', function () {
     TweenMax.to("#cta2", 1, { x: '-20vw', opacity:0})
     TweenMax.to("#cta3", 1, { x: '-40vw', opacity:0})
     TweenMax.to("#cta4", 1, { x: '-60vw', opacity:0})
+    TweenMax.to("#cta1", 1, { x: window.screen.width / 1.75, delay: 1.5})
 })
 
 document.getElementById('cta2').addEventListener('click', function () {
     TweenMax.to("#cta1", 1, { x: '20vw', opacity:0})
     TweenMax.to("#cta3", 1, { x: '-20vw', opacity:0})
     TweenMax.to("#cta4", 1, { x: '-40vw', opacity:0})
+    TweenMax.to("#cta2", 1, { x: window.screen.width / 8, delay: 1.5})
 })
 
 document.getElementById('cta3').addEventListener('click', function () {
     TweenMax.to("#cta1", 1, { x: '40vw', opacity:0})
     TweenMax.to("#cta2", 1, { x: '20vw', opacity:0})
     TweenMax.to("#cta4", 1, { x: '-20vw', opacity:0})
+    TweenMax.to("#cta3", 1, { x: -window.screen.width / 4, delay: 1.5})
 })
 
 document.getElementById('cta4').addEventListener('click', function () {
     TweenMax.to("#cta1", 1, { x: '60vw', opacity:0})
     TweenMax.to("#cta2", 1, { x: '40vw', opacity:0})
     TweenMax.to("#cta3", 1, { x: '20vw', opacity:0})
+    TweenMax.to("#cta4", 1, { x: -window.screen.width/ 1.6, delay: 1.5})
 })
 
 document.getElementById('title-nemesis').addEventListener('click', function () {
