6337e71dd0839d4d9c408787ab2b2a0c6765f951
MikeTheFyke <mikefyke@hotmail.com>
2020-10-19 20:05:34 -0400

    Added event listener to cta buttons

--- logic/applesLogic.js
+++ logic/applesLogic.js
@@ -2,3 +2,8 @@ TweenMax.to("#title-nemesis", 0.5, { y: '410px',ease: Elastic.easeOut, delay: 0.
 TweenMax.to("#title-authors", 0.5, { y: '100px', delay: 1});
 
 TweenMax.to(".cta", 0.5, { scaleX: 1, delay: 1.5});
+
+document.getElementById('cta1').addEventListener('click', function () {
+    TweenMax.to(".cta", 1, { opacity:0})
+
+})
