e302f1c4d2a9d85fd7040fae9dceb90b3dc72f16
MikeTheFyke <mikefyke@hotmail.com>
2020-10-26 20:58:38 -0400

    Adde page turn logic and page counter

--- logic/nemesis1Logic.js
+++ logic/nemesis1Logic.js
@@ -1,3 +1,6 @@
+var page = 1;
+
 function nemesis1 () {
-    TweenMax.to("#issue1-cover", 0.5, { content:'url("../images/Nemesis/Nemesis01/Nemesis01-page01.jpg")', delay: 1.5});
+    TweenMax.to("#issue1-cover", 0.5, { content:'url("../images/Nemesis/Nemesis01/Nemesis01-page'+ page + '.jpg")'});
+    page ++;
 }
