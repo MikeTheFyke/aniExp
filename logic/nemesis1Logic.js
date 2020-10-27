603caeb56960eec4f461f53af4a773ba49fd9100
MikeTheFyke <mikefyke@hotmail.com>
2020-10-26 21:06:07 -0400

    Added conditional to issue 1

--- logic/nemesis1Logic.js
+++ logic/nemesis1Logic.js
@@ -2,5 +2,10 @@ var page = 1;
 
 function nemesis1 () {
     TweenMax.to("#issue1-cover", 0.5, { content:'url("../images/Nemesis/Nemesis01/Nemesis01-page'+ page + '.jpg")'});
-    page ++;
+    if (page >= 23){
+        TweenMax.to("#issue1-cover", 0.5, { content:'url("../images/Nemesis/Nemesis1TextLessCover.png")'});
+        page = 1;
+    } else {
+        page ++;
+    }
 }
