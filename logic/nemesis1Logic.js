a8ca201b39a0a3925fcd9030cb107c6a0b197a18
MikeTheFyke <mikefyke@hotmail.com>
2020-10-27 11:22:33 -0400

    Fixed Timing of page image source change

--- logic/nemesis1Logic.js
+++ logic/nemesis1Logic.js
@@ -1,9 +1,9 @@
 var page = 1;
 
 function nemesis1 () {
-    TweenMax.to("#issue1-cover", 0.5, { content:'url("../images/Nemesis/Nemesis01/Nemesis01-page'+ page + '.jpg")'});
+    TweenMax.to("#issue1-cover", 0, { content:'url("../images/Nemesis/Nemesis01/Nemesis01-page'+ page + '.jpg")'});
     if (page >= 23){
-        TweenMax.to("#issue1-cover", 0.5, { content:'url("../images/Nemesis/Nemesis1TextLessCover.png")'});
+        TweenMax.to("#issue1-cover", 0, { content:'url("../images/Nemesis/Nemesis1TextLessCover.png")'});
         page = 1;
     } else {
         page ++;
